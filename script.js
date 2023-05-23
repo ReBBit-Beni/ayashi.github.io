dae90a10086be0da21260ac8a3be87629c06a7b4
Rebbit <raychin38@icloud.com>
2023-05-23 16:42:25 +0900

    音声の追加

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function() {
+  var audio = new Audio('Audio/Cyber20-1.mp3'); // 音声ファイルのパスを指定します
+  console.log('音声ファイルの再生を開始します'); // ログメッセージを出力
+  audio.play();
+});
